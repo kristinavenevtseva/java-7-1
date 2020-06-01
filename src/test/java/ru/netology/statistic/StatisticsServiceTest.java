ef117d255f61314d481dd201282058030aa954cd
kristinavenevtseva <63412659+kristinavenevtseva@users.noreply.github.com>
2020-06-01 12:35:44 +0300

    change pom.xml

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -2,12 +2,12 @@ package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxIncomesNotZero() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
@@ -17,4 +17,5 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
 }
